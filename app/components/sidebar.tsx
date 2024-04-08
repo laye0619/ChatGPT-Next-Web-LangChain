7fb3e9b273d93f1ac14ee9a3c4e4f48972d5a850
laye0619 <laye0619@gmail.com>
2024-04-08 21:56:09 +0800

    Customizing to Yo AI

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,10 +174,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Yo AI Chat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Powered by Google Gemini Pro
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Yo AI Chat",
+  description: "Powered by Google Gemini Pro",
   viewport: {
     width: "device-width",
     initialScale: 1,
